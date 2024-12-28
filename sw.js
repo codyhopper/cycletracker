0ed7c9317b65ade89dc65b153e8ba118dbc55001
Cody Hopper <hello@codyhopper.com>
2024-12-27 19:37:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,77 +1,65 @@
-// service worker
-// TODOS: include syncing and push: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Offline_and_background_operation
-
-// unique identifier
+// The version of the cache.
 const VERSION = "v1";
 
-// application cache name
+// The name of the cache
 const CACHE_NAME = `period-tracker-${VERSION}`;
 
-// list of offline resources
-// TODO: include any resources statically included in the app
+// The static resources that the app needs to function.
 const APP_STATIC_RESOURCES = [
-    "/",
-    "/index.html", 
-    "/style.css",
-    "/app.js",
-    "/cycletracker.json",
-    "/icons/wheel.svg",
-]
+  "/",
+  "/index.html",
+  "/app.js",
+  "/style.css",
+  "/icons/wheel.svg",
+];
 
-// on install, cache static resources
+// On install, cache the static resources
 self.addEventListener("install", (event) => {
-    // .waitUntill() is a request to the browser to not terminate the service worker while a task is being executed and promise settles
-    event.waitUntil(
-        (async () => {
-            // .open() returns a promise that resolves to the cache object matching the name
-            const cache = await caches.open(CACHE_NAME);
-            
-            // .addAll() adds responses to the given cache
-            cache.addAll(APP_STATIC_RESOURCES);
-        })(),
-    );
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })()
+  );
 });
 
-// on activate, delete old caches and avoid running out of space
+// delete old caches on activate
 self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        (async () => {
-            const names = await caches.keys();
-            await Promise.all(
-                names.map((name) => {
-                if (name !== CACHE_NAME) {
-                    return caches.delete(name);
-                }
-            }),
-        );
-            await clients.claim();
-        })(),
-    );
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+      await clients.claim();
+    })()
+  );
 });
 
-// on fetch, intercept server requests and respond with cached responses instead of going to server
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
 self.addEventListener("fetch", (event) => {
-    // when seeking an HTML page
-    if (event.request.mode === "navigate") {
-        // Return to the index.html page as single-page app
-        event.respondWith(caches.match("/"));
-        return;
-    }
-  
-    // For every other request type, go to cache first, then network
-    event.respondWith(
-		(async () => {
-			const cache = await caches.open(CACHE_NAME);
-			const cachedResponse = await cache.match(event.request.url);
-			if (cachedResponse) {
-				// Return the cached response if it's available.
-				return cachedResponse;
-			}
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
 
-        	// Respond with a HTTP 404 response status.
-        	return new Response(null, { 
-				status: 404 
-			});
-		})(),
-	);
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })()
+  );
 });
